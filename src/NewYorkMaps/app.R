f6508499cc3380120c7e3d3da310af11fff5197c
abemelech <abemelech.mesfin@gmail.com>
2023-05-11 18:01:48 -0500

    added distance

--- src/NewYorkMaps/app.R
+++ src/NewYorkMaps/app.R
@@ -4,10 +4,26 @@ library(leaflet)
 library(readr)
 library(dplyr)
 library(tidyr)  # for replace_na
+library(geosphere)
 
 # Define UI
 ui <- fluidPage(
+  
   titlePanel("Garden and Grocery Locations"),
+  
+  sidebarLayout(
+    sidebarPanel( 
+      bottom = 10, right = 10,
+      sliderInput("lat", "Latitude", 40.705, min = 40.49, max = 40.92),
+      sliderInput("lng", "Longitude", -73.975, min = -74.27, max=-73.68),
+      actionButton("calculate", "Calculate")
+    ),
+    mainPanel(
+      leafletOutput("map", height = 550), # adjust the map Size to take up the whole page
+      textOutput("grocery_dist"),
+      textOutput("garden_dist"),
+    )
+  ),
   
   absolutePanel(bottom = 10, right = 10, # place the panel in the top right corner
                 fileInput('file1', 'Choose Garden CSV File',
@@ -23,7 +39,7 @@ ui <- fluidPage(
                 checkboxInput('header2', 'Header', TRUE)
   ),
   
-  leafletOutput("map", height = 550) # adjust the map Size to take up the whole page
+  
 )
 
 # Define server logic
@@ -57,28 +73,71 @@ server <- function(input, output) {
       filter(!is.na(Longitude), !is.na(Latitude))
   })
   
-  output$map <- renderLeaflet({
-    gardenData <- gardenData()
-    groceryData <- groceryData()
-    
-    # Create an empty map to start with
-    map <- leaflet() |> addTiles()
-    
-    # If garden data is loaded, add it to the map
-    if (!is.null(gardenData)) {
+  output$grocery_dist <- renderText({ "The closest grocery store is" }) # initialize the output
+  output$garden_dist <- renderText({ "The closest garden is" }) # initialize the output
+  
+  observeEvent(input$calculate, {
+    output$map <- renderLeaflet({
+      gardenData <- gardenData()
+      groceryData <- groceryData()
+      
+      # Create an empty map to start with
+      map <- leaflet() |> addTiles()
+      
+      # If garden data is loaded, add it to the map
+      if (!is.null(gardenData)) {
+        map <- map |> 
+          addCircleMarkers(data = gardenData, lng = ~Longitude, lat = ~Latitude, popup = ~`Garden Name`, radius = ~Size * 5) 
+      }
+      
+      # If grocery data is loaded, add it to the map
+      if (!is.null(groceryData)) {
+        map <- map |> 
+          addMarkers(data = groceryData, lng = ~Longitude, lat = ~Latitude, popup = ~`Entity Name`,
+                     icon = ~leaflet::makeIcon(iconUrl = 'shop-icon.png', iconWidth = 10, iconHeight = 10)) 
+      }
+      
+      # Add a FontAwesome user icon at the user-specified coordinates
       map <- map |> 
-        addCircleMarkers(data = gardenData, lng = ~Longitude, lat = ~Latitude, popup = ~`Garden Name`, radius = ~Size * 5) 
+        addAwesomeMarkers(lng = input$lng, lat = input$lat, icon = awesomeIcons(icon = 'user', library = 'fa', markerColor = 'red'))
+      
+      # Set the map view to the user's input latitude and longitude
+      map |> setView(lng = input$lng, lat = input$lat, zoom = 10)
+      
+      map
+    })
+    
+    if (!is.null(groceryData())) {
+      # Calculate the grocery_dists from the user-specified coordinates to all grocery stores
+      grocery_dists <- distm(c(input$lng, input$lat), groceryData()[, c("Longitude", "Latitude")], fun = distVincentySphere)
+      
+      # Find the minimum grocery_dist
+      min_grocery_dist <- min(grocery_dists)
+      
+      # Convert the grocery_dist to kilometers
+      min_grocery_dist_km <- min_grocery_dist / 1000
+      
+      # Update the output
+      output$grocery_dist <- renderText({
+        paste0("The closest grocery store is ", round(min_grocery_dist_km, 2), " kilometers away.")
+      })
     }
     
-    # If grocery data is loaded, add it to the map
-    # Need to fix grocery icons
-    if (!is.null(groceryData)) {
-      map <- map |> 
-        addMarkers(data = groceryData, lng = ~Longitude, lat = ~Latitude, popup = ~`Entity Name`,
-                   icon = ~leaflet::makeIcon(iconUrl = 'shop-icon.png', iconWidth = 10, iconHeight = 10)) 
+    if (!is.null(gardenData())) {
+      # Calculate the grocery_dists from the user-specified coordinates to all grocery stores
+      garden_dists <- distm(c(input$lng, input$lat), gardenData()[, c("Longitude", "Latitude")], fun = distVincentySphere)
+      
+      # Find the minimum grocery_dist
+      min_garden_dist <- min(garden_dists)
+      
+      # Convert the grocery_dist to kilometers
+      min_garden_dist_km <- min_garden_dist / 1000
+      
+      # Update the output
+      output$garden_dist <- renderText({
+        paste0("The closest garden is ", round(min_garden_dist_km, 2), " kilometers away.")
+      })
     }
-    
-    map
   })
 }
 
